12aaf2d8b79cd94ffc2f57eafa5f36e8b3ca32ac
IbrahimBu11 <ibrahim.alibu11work@gmail.com>
2023-11-01 13:35:34 +0500

    Characters

---
+++ Assets/AddComponentScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using CoverShooter;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AddComponentScript : MonoBehaviour
+{
+    private Rigidbody[] bodyhealths;
+    
+    [ContextMenu("AddBodyPartHealth")]
+    private void AddComponent()
+    {
+        bodyhealths = transform.GetComponentsInChildren<Rigidbody>();
+        
+
+        foreach (var VARIABLE in bodyhealths)
+        {
+            VARIABLE.isKinematic = true;
+            
+            if(VARIABLE.transform.GetComponent<BodyPartHealth>() == null)
+                VARIABLE.transform.AddComponent<BodyPartHealth>();
+            
+            if(VARIABLE != null)
+             if(VARIABLE.GetComponent<CharacterJoint>() != null)
+                 DestroyImmediate(VARIABLE.GetComponent<CharacterJoint>());
+            
+            DestroyImmediate(VARIABLE);
+
+        }
+    }
+
+    [ContextMenu("Collider Trigger")]
+    private void DisableKinematic()
+    {
+        Collider[] col = transform.GetComponentsInChildren<Collider>();
+
+        
+        foreach (var VARIABLE in col)
+        {
+            VARIABLE.isTrigger = true;
+        }
+    }
+}
